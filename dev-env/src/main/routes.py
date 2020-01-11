d8801fe4786513bca8e1bc0e2b063b850e4d2e1d
Alex Shelton <alexshelton@Zoso.hsd1.il.comcast.net>
2020-01-10 23:40:56 -0600

    Building http api

--- dev-env/src/main/routes.py
+++ dev-env/src/main/routes.py
@@ -17,8 +17,7 @@ def home():
     legend = 'Weekly Data'
     labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     values = [10,15,23,12,32,17,25]
-    maxBar = max(values)
-    return render_template('home.html', values=values, labels=labels, legend=legend, max=maxBar)
+    return render_template('home.html', values=values, labels=labels, legend=legend)
 
 
 @main.route('/about')
